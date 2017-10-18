bc4f9ae4909e4a48106797936e09a526285ca672
zoe <zoe@offerhk.com>
2017-10-18 18:44:59 +0800

    通道添加

--- src/channels_t.go
+++ src/channels_t.go
@@ -10,20 +10,8 @@ import (
 )
 
 func main() {
-	// ===============================通道========================
-	// 使用 `make(chan val-type)` 创建一个新的通道。
-	// 通道类型就是他们需要传递值的类型。
-	messages := make(chan string)
-	go func() {
-		// 使用 `channel <-` 语法 _发送_ 一个新的值到通道中。这里
-		// 我们在一个新的 Go 协程中发送 `"ping"` 到上面创建的
-		// `messages` 通道中。
-		messages <- "ping"
-	}()
-	// 使用 `<-channel` 语法从通道中 _接收_ 一个值.
-	msg := <-messages
-	// 将接收我们在上面发送的 `"ping"` 消息并打印出来。
-	fmt.Println(msg)
+	// ===============================通道实例========================
+	channels()
 	//========================通道方向===============================
 	pings := make(chan string, 1)
 	pongs := make(chan string, 1)
@@ -53,6 +41,44 @@ func main() {
 	// Go 的_通道选择器_ 让你可以同时等待多个通道操作。
 	// Go 协程和通道以及选择器的结合是 Go 的一个强大特性。
 	chanSelector()
+	// ==========================超时处理=============================
+	// _超时_
+	// 对于一个连接外部资源，或者其它一些需要花费执行时间的操作的程序而言是很重要的。
+	// 得益于通道和 `select`，在 Go中实现超时操作是简洁而优雅的。
+	timeouts()
+	// ==========================非阻塞通道操作=============================
+	// 常规的通过通道发送和接收数据是阻塞的。
+	// 然而，我们可以使用带一个 `default` 子句的 `select`
+	// 来实现_非阻塞_ 的发送、接收，
+	// 甚至是非阻塞的多路 `select`(意思是select 嵌套？)。
+	nonBlockingChannel()
+	// ==========================通道的关闭=============================
+	// _关闭_ 一个通道意味着不能再向这个通道发送值了。
+	// 这个特性可以用来给这个通道的接收方传达工作已将完成的信息。
+	done2 := make(chan bool, 1)
+	closeChannel(done2)
+	<-done2
+}
+
+// =========================通道实例========================================
+/**
+ * 通道实例
+ * @return {[type]} [description]
+ */
+func channels() {
+	// 使用 `make(chan val-type)` 创建一个新的通道。
+	// 通道类型就是他们需要传递值的类型。
+	messages := make(chan string)
+	go func() {
+		// 使用 `channel <-` 语法 _发送_ 一个新的值到通道中。这里
+		// 我们在一个新的 Go 协程中发送 `"ping"` 到上面创建的
+		// `messages` 通道中。
+		messages <- "ping"
+	}()
+	// 使用 `<-channel` 语法从通道中 _接收_ 一个值.
+	msg := <-messages
+	// 将接收我们在上面发送的 `"ping"` 消息并打印出来。
+	fmt.Println(msg)
 }
 
 // =========================通道方向========================================
@@ -115,4 +141,113 @@ func chanSelector() {
 			fmt.Println("reveived", msg2)
 		}
 	}
+}
+
+// =========================超时处理========================================、
+/**
+ * 使用`select` 超时方式，需要使用通道传递结果。
+ * 这对于一般情况是个好的方式，因为其他重要的 Go 特性是基于通道和`select` 的。
+ * @return {[type]} [description]
+ */
+func timeouts() {
+	// 假如我们执行一个外部调用，并在 2 秒后通过通道 `c1` 返回它的执行结果。
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "result 1"
+	}()
+	// 这里是使用 `select` 实现一个超时操作。
+	// `res := <- c1` 等待结果，`<-Time.After` 等待超时 时间 1 秒后发送的值。
+	// 由于 `select` 默认处理第一个已准备好的接收操作，
+	// 如果这个操作超过了允许的 1 秒的话，将会执行超时case。
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second * 1):
+		fmt.Println("timeout 1")
+	}
+
+	// 如果我允许一个长一点的超时时间 3 秒，将会成功的从 `c2`
+	// 接收到值，并且打印出结果。
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(time.Second * 3):
+		fmt.Println("timeout 2")
+	}
+}
+
+// =========================非阻塞通道操作========================================
+/**
+ * 非阻塞通道接受，发送实例
+ * @return {[type]} [description]
+ */
+func nonBlockingChannel() {
+	messages := make(chan string)
+	signals := make(chan bool)
+	// 这里是一个非阻塞`接收`的例子。
+	// 如果在 `messages` 中存在，然后 `select` 将这个值带入 `<-messages` `case`。
+	// 如果不是，就直接到 `default` 分支中。
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+	// 一个非阻塞`发送`的实现方法。
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+	// 我们可以在 `default` 前使用多个 `case` 子句来实现一个多路的非阻塞的选择器。
+	// 这里我们试图在 `messages`和 `signals` 上同时使用非阻塞的接受操作。
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
+
+// =========================通道的关闭========================================
+func closeChannel(done chan bool) {
+	jobs := make(chan int, 5)
+	// 这是工作 Go 协程。
+	// 使用 `j, more := <- jobs` 循环的从 `jobs` 接收数据。
+	// 在接收的这个特殊的二值形式的值中，
+	// 如果 `jobs` 已经关闭了，并且通道中所有的值都已经接收完毕, 那么 `more` 的值将是 `false`。
+	// 当我们完成所有的任务时，将使用这个特性通过 `done` 通道去进行通知。
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				//发送一个值通知已经完成
+				done <- true
+				return
+			}
+		}
+	}()
+	// 这里使用 `jobs` 发送 3 个任务到工作函数中，然后
+	// 关闭 `jobs`。
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+	// 我们使用前面学到的[通道同步]
+	// 方法等待任务结束。
 }
